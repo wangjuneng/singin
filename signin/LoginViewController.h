ef8472680e41a1b4fc0870f4d2b6cefa07c89aff
wangjuneng <wangjuneng@126.com>
2015-06-04 16:13:52 +0800

    LoginViewController

--- signin/LoginViewController.h
+++ signin/LoginViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  LoginViewController.h
 //  signin
 //
 //  Created by apple on 15/6/4.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface LoginViewController : UIViewController
 
 @end
-
