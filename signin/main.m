aabae08a902b8fce226387b6bd488c27c7bdd96e
wangjuneng <wangjuneng@126.com>
2015-06-04 15:05:52 +0800

    project init

---
+++ signin/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  signin
+//
+//  Created by apple on 15/6/4.
+//  Copyright (c) 2015年 gense. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
